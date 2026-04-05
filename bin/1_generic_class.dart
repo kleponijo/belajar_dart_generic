3011ac53602d9f5eebd8ceefa2f4122c7d4a6ff1
kleponijo <158453666+kleponijo@users.noreply.github.com>
2026-04-05 19:29:53 +0700

    oke

--- bin/1_generic_class.dart
+++ bin/1_generic_class.dart
@@ -1,9 +1,1 @@
-import 'data/1_mydata.dart';
 
-void main() {
-  var dataString = MyData<String>("Nadif Zidan Tazaka");
-  var dataInt = MyData(100);
-  var dataBool = MyData(true);
-
-  print(dataString.data);
-}
--- bin/data/1_mydata.dart
+++ bin/data/1_mydata.dart
@@ -1,10 +1,1 @@
-class MyData<T> {
-  T data;
-  MyData(this.data);
 
-  void test(T data) {}
-
-  T getData() {
-    return data;
-  }
-}
